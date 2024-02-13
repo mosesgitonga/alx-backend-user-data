9f10448146cb8a5e23bce510584757fd48fd0d63
mosesgitonga <infosec947@gmail.com>
2024-02-13 18:01:33 +0300

    getting user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -47,3 +47,19 @@ class BasicAuth:
                 return False
         except Exception:
             pass
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        get the user credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        
+        username, password = decoded_base64_authorization_header.split(':', 1)
+        return username, password
