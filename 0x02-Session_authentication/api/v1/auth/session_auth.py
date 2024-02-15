cb62bf3289b2732c39c8eb35edc95cd1481864bc
mosesgitonga <infosec947@gmail.com>
2024-02-15 16:56:38 +0300

    creating a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,14 +3,30 @@
 session authentication
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     session authentication mechanism
     """
+    user_id_by_session_id = {}
+
     def __init__(self):
         pass
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
+
 
 if __name__ == '__main__':
     session_auth = SessionAuth()
