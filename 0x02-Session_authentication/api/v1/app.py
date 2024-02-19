e1189eb94a78326e61c143887b19dbee2e1cbc92
mosesgitonga <infosec947@gmail.com>
2024-02-19 09:22:11 +0300

    changes

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,7 +9,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+from os import getenv
 
 
 app = Flask(__name__)
@@ -17,14 +17,23 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth_type = os.getenv('AUTH_TYPE')
+AUTH_TYPE = getenv("AUTH_TYPE")
 
-if auth_type == "basic_auth":
-    auth = BasicAuth()
-elif auth_type == 'auth':
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
-elif auth_type == 'session_auth':
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(403)
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,46 +6,50 @@ from typing import Tuple
 
 from flask import abort, jsonify, request
 
-from api.v1.app import auth
+
 from api.v1.views import app_views
 from models.user import User
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def session_auth_login() -> Tuple[str, int]:
-    """POST /api/v1/auth_session/login.
+def session_auth_login():
 
-    Returns:
-        - JSON representation of a User object.
-    """
-    # Get the email and password values from the form data
     email = request.form.get('email')
     password = request.form.get('password')
-    # Return an error if the email is missing or empty
-    if not email:
+    # If email is missing, return a JSON response with the status code 400
+    if email is None:
         return jsonify({"error": "email missing"}), 400
-    # Return an error if the password is missing or empty
-    if not password:
+    
+    # If password is missing, return a JSON response with the status code 400
+    if password is None:
         return jsonify({"error": "password missing"}), 400
-    # Retrieve the User instance based on the email
-    user = User.search({'email': email})
-    # Return an error if no User was found
-    if not user:
+
+    try:
+        found_users = User.search({'email': email})
+    except Exception:
         return jsonify({"error": "no user found for this email"}), 404
-    # Return an error if the password is incorrect
-    if not user[0].is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    # Otherwise, create a Session ID for the User ID
-    # You must use auth.create_session(..) for creating a Session ID
-    session_id = auth.create_session(getattr(user[0], 'id'))
-    # Return the User in JSON format
-    response = jsonify(user[0].to_json())
-    # Set the cookie in the response
-    response.set_cookie(os.getenv("SESSION_NAME"), session_id)
-    # Return the response with the User and the cookie
-    return response
 
+    if found_users:
+        user = found_users[0]
+    else:
+        return jsonify({"error": "no user found for this email"}), 404
 
+    for user in found_users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
+    user = found_users[0]
+    
+    from api.v1.app import auth
+
+    SESSION_NAME = os.getenv('SESSION_NAME')
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(SESSION_NAME, session_id)
+    return response, 200
+
+    
+    
 @app_views.route(
     '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def session_auth_logout():
@@ -54,6 +58,7 @@ def session_auth_logout():
     Returns:
         - An empty JSON object.
     """
+    from api.v1.app import auth
     # You must use auth.destroy_session(request) for deleting the Session ID
     is_destroyed = auth.destroy_session(request)
     # If destroy_session returns False, abort(404)
