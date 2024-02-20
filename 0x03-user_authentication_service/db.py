e2c6cff0f11fce94d8db0e29079c806c7d8a9c6b
mosesgitonga <infosec947@gmail.com>
2024-02-20 15:58:38 +0300

    filter

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -38,3 +38,19 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        return first row of user table
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            if user is None:
+                return 'err'
+            return user
+        except NoResultFound as e:
+            raise  NoResultFound('No result') 
+        except InvalidRequestError as e:
+            raise InvalidRequestError("Invalid query arguments")
+        except Exception as e:
+            return e
