6aa92f7b42e887816c1a9823b73b6ba687b1673c
mosesgitonga <infosec947@gmail.com>
2024-02-21 17:26:09 +0300

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 class Auth:
@@ -53,6 +54,13 @@ class Auth:
             return True
         else:
             return False
+
+    def _generate_uuid(self) -> str:
+        """
+        return a string representation of a new uuid
+        """
+        uuid = str(uuid4())
+        return uuid
 
 
 def _hash_password(password: str) -> bytes:
