0877b7c96c48dac65b27f3498fe6ad6333d00db0
mosesgitonga <infosec947@gmail.com>
2024-02-22 15:49:19 +0300

    corrected the route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -101,7 +101,7 @@ def get_reset_password_token():
     response = jsonify({"email": email, "reset_token": token})
     return response, 200
 
-@app.route('/reset_passowrd', methods=['PUT'])
+@app.route('/reset_password', methods=['PUT'])
 def update_password():
     """updating password
     """
