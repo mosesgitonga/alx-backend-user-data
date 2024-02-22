5de88e78c386d1c9f6fbe94a68406bce0d522961
mosesgitonga <infosec947@gmail.com>
2024-02-22 15:22:13 +0300

    m,inor chage

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -139,6 +139,9 @@ class Auth:
             raise ValueError
 
     def update_user(self, reset_token: str, password: str) -> None:
+        """
+        updating the passowrd through the reset_token
+        """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except ValueError:
