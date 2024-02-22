67ab35790f9f2b7735dbb351ff595a26be62f48f
mosesgitonga <infosec947@gmail.com>
2024-02-22 15:50:55 +0300

    solved pycodestyles

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -101,6 +101,7 @@ def get_reset_password_token():
     response = jsonify({"email": email, "reset_token": token})
     return response, 200
 
+
 @app.route('/reset_password', methods=['PUT'])
 def update_password():
     """updating password
@@ -119,6 +120,7 @@ def update_password():
 
     msg = {"email": email, "message": "Password updated"}
     return jsonify(msg), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
